925990a611b1ed4997207db18cf62eeeb136ec6c
gabriellagziel <gabriellagziel@gmail.com>
2025-06-11 23:00:13 +0200

    Add studio mode booking

---
+++ lib/providers/studio_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../services/studio_service.dart';
+import '../models/staff_member.dart';
+import '../models/staff_availability.dart';
+
+final studioServiceProvider = Provider<StudioService>((ref) => StudioService());
+
+final staffListProvider =
+    FutureProvider.family<List<StaffMember>, String>((ref, studioId) {
+  return ref.read(studioServiceProvider).fetchStaff(studioId);
+});
+
+final staffAvailabilityProvider =
+    FutureProvider.family<StaffAvailability, Map<String, dynamic>>(
+        (ref, params) {
+  return ref
+      .read(studioServiceProvider)
+      .fetchAvailability(params['staffId'] as String, params['date'] as DateTime);
+});
