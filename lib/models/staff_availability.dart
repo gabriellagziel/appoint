430b73297f6e169798d78982f10d03dd645ec4f9
gabriellagziel <gabriellagziel@gmail.com>
2025-06-13 22:57:38 +0200

    fix: avoid null operator crash in StaffAvailability

--- lib/models/staff_availability.dart
+++ lib/models/staff_availability.dart
@@ -6,13 +6,15 @@ import 'time_of_day_converter.dart';
 part 'staff_availability.freezed.dart';
 part 'staff_availability.g.dart';
 
+String _dateToIso(DateTime d) => d.toIso8601String();
+
 @freezed
 class StaffAvailability with _$StaffAvailability {
   const factory StaffAvailability({
     required String staffId,
-    @JsonKey(fromJson: DateTime.parse, toJson: (DateTime d) => d.toIso8601String())
+    @JsonKey(fromJson: DateTime.parse, toJson: _dateToIso)
     required DateTime date,
-    // TODO: Re-add converter after generator compatibility is fixed
+    // TODO: Re-add TimeOfDayConverter once generator issues are resolved
     @TimeOfDayConverter() required List<TimeOfDay> availableSlots,
   }) = _StaffAvailability;
 
