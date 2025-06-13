c072c4f2278ada6b55b66b6b7b119073eb2940aa
gabriellagziel <gabriellagziel@gmail.com>
2025-06-13 22:57:38 +0200

    fix: avoid null operator crash in StaffAvailability

---
+++ lib/models/staff_availability.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'staff_availability.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$StaffAvailabilityImpl _$$StaffAvailabilityImplFromJson(
+        Map<String, dynamic> json) =>
+    _$StaffAvailabilityImpl(
+      staffId: json['staffId'] as String,
+      date: DateTime.parse(json['date'] as String),
+      availableSlots: (json['availableSlots'] as List<dynamic>)
+          .map((e) => const TimeOfDayConverter().fromJson(e as String))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$StaffAvailabilityImplToJson(
+        _$StaffAvailabilityImpl instance) =>
+    <String, dynamic>{
+      'staffId': instance.staffId,
+      'date': _dateToIso(instance.date),
+      'availableSlots': instance.availableSlots
+          .map(const TimeOfDayConverter().toJson)
+          .toList(),
+    };
