56b0ff5201844fee5e7e406552f56ebe12da5a03
gabriellagziel <gabriellagziel@gmail.com>
2025-06-15 20:18:25 +0200

    fix: rebuild staff_availability model with DateTime support and Freezed

--- lib/models/staff_availability.dart
+++ lib/models/staff_availability.dart
@@ -1,23 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-
-import 'time_of_day_converter.dart';
-
-// ignore_for_file: invalid_annotation_target
+import '../utils/datetime_converter.dart';
 
 part 'staff_availability.freezed.dart';
 part 'staff_availability.g.dart';
-
-String _dateToIso(DateTime d) => d.toIso8601String();
 
 @freezed
 class StaffAvailability with _$StaffAvailability {
   const factory StaffAvailability({
     required String staffId,
-    @JsonKey(fromJson: DateTime.parse, toJson: _dateToIso)
-    required DateTime date,
-    // TODO: Re-add TimeOfDayConverter once generator issues are resolved
-    @TimeOfDayConverter() required List<TimeOfDay> availableSlots,
+
+    @DateTimeConverter()
+    required DateTime availableFrom,
+
+    @DateTimeConverter()
+    required DateTime availableTo,
   }) = _StaffAvailability;
 
   factory StaffAvailability.fromJson(Map<String, dynamic> json) =>
