01e25b03fc5eaccceb0a432e3513a22003caade3
gabriellagziel <gabriellagziel@gmail.com>
2025-06-15 17:54:37 +0200

    fix: resolved all DateTime-related errors across models and features

---
+++ lib/models/booking.freezed.dart
@@ -0,0 +1,200 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'booking.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Booking _$BookingFromJson(Map<String, dynamic> json) {
+  return _Booking.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Booking {
+  String get id => throw _privateConstructorUsedError;
+  @JsonKey(fromJson: _fromJson, toJson: _toJson)
+  DateTime get startTime => throw _privateConstructorUsedError;
+  @JsonKey(fromJson: _fromJson, toJson: _toJson)
+  DateTime get endTime => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $BookingCopyWith<Booking> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BookingCopyWith<$Res> {
+  factory $BookingCopyWith(Booking value, $Res Function(Booking) then) =
+      _$BookingCopyWithImpl<$Res, Booking>;
+  @useResult
+  $Res call(
+      {String id,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson) DateTime startTime,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson) DateTime endTime});
+}
+
+/// @nodoc
+class _$BookingCopyWithImpl<$Res, $Val extends Booking>
+    implements $BookingCopyWith<$Res> {
+  _$BookingCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? startTime = null,
+    Object? endTime = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      startTime: null == startTime
+          ? _value.startTime
+          : startTime // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      endTime: null == endTime
+          ? _value.endTime
+          : endTime // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$BookingImplCopyWith<$Res> implements $BookingCopyWith<$Res> {
+  factory _$$BookingImplCopyWith(
+          _$BookingImpl value, $Res Function(_$BookingImpl) then) =
+      __$$BookingImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson) DateTime startTime,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson) DateTime endTime});
+}
+
+/// @nodoc
+class __$$BookingImplCopyWithImpl<$Res>
+    extends _$BookingCopyWithImpl<$Res, _$BookingImpl>
+    implements _$$BookingImplCopyWith<$Res> {
+  __$$BookingImplCopyWithImpl(
+      _$BookingImpl _value, $Res Function(_$BookingImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? startTime = null,
+    Object? endTime = null,
+  }) {
+    return _then(_$BookingImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      startTime: null == startTime
+          ? _value.startTime
+          : startTime // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      endTime: null == endTime
+          ? _value.endTime
+          : endTime // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$BookingImpl implements _Booking {
+  _$BookingImpl(
+      {required this.id,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson) required this.startTime,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson) required this.endTime});
+
+  factory _$BookingImpl.fromJson(Map<String, dynamic> json) =>
+      _$$BookingImplFromJson(json);
+
+  @override
+  final String id;
+  @override
+  @JsonKey(fromJson: _fromJson, toJson: _toJson)
+  final DateTime startTime;
+  @override
+  @JsonKey(fromJson: _fromJson, toJson: _toJson)
+  final DateTime endTime;
+
+  @override
+  String toString() {
+    return 'Booking(id: $id, startTime: $startTime, endTime: $endTime)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$BookingImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.startTime, startTime) ||
+                other.startTime == startTime) &&
+            (identical(other.endTime, endTime) || other.endTime == endTime));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, startTime, endTime);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$BookingImplCopyWith<_$BookingImpl> get copyWith =>
+      __$$BookingImplCopyWithImpl<_$BookingImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$BookingImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Booking implements Booking {
+  factory _Booking(
+      {required final String id,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson)
+      required final DateTime startTime,
+      @JsonKey(fromJson: _fromJson, toJson: _toJson)
+      required final DateTime endTime}) = _$BookingImpl;
+
+  factory _Booking.fromJson(Map<String, dynamic> json) = _$BookingImpl.fromJson;
+
+  @override
+  String get id;
+  @override
+  @JsonKey(fromJson: _fromJson, toJson: _toJson)
+  DateTime get startTime;
+  @override
+  @JsonKey(fromJson: _fromJson, toJson: _toJson)
+  DateTime get endTime;
+  @override
+  @JsonKey(ignore: true)
+  _$$BookingImplCopyWith<_$BookingImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
