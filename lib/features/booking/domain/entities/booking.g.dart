7004ebf1629f24bbc526116857bd0077dcfc24c2
gabriellagziel <gabriellagziel@gmail.com>
2025-06-15 15:49:46 +0200

    fix: regenerate booking & tmp_test models with DateTime

---
+++ lib/features/booking/domain/entities/booking.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'booking.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$BookingImpl _$$BookingImplFromJson(Map<String, dynamic> json) =>
+    _$BookingImpl(
+      id: json['id'] as String,
+      startTime: DateTime.parse(json['startTime'] as String),
+      endTime: DateTime.parse(json['endTime'] as String),
+    );
+
+Map<String, dynamic> _$$BookingImplToJson(_$BookingImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'startTime': instance.startTime.toIso8601String(),
+      'endTime': instance.endTime.toIso8601String(),
+    };
