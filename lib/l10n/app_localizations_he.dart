69c30f82246aa8895cea150781f688956112a405
gabriellagziel <gabriellagziel@gmail.com>
2025-06-16 18:18:22 +0200

    fix: synchronize all code to eliminate analyzer issues

---
+++ lib/l10n/app_localizations_he.dart
@@ -0,0 +1,15 @@
+import 'app_localizations.dart';
+
+/// The translations for Hebrew (`he`).
+class AppLocalizationsHe extends AppLocalizations {
+  AppLocalizationsHe([String locale = 'he']) : super(locale);
+
+  @override
+  String get appTitle => 'אפפוינט';
+
+  @override
+  String get welcome => 'ברוך הבא';
+
+  @override
+  String get bookMeeting => 'קבע פגישה';
+}
