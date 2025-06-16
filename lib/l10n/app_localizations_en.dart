4a08a8d69ff8ebc9c00db7b7321427a8143ac7e7
gabriellagziel <gabriellagziel@gmail.com>
2025-06-16 18:18:22 +0200

    fix: synchronize all code to eliminate analyzer issues

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,15 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Appoint';
+
+  @override
+  String get welcome => 'Welcome';
+
+  @override
+  String get bookMeeting => 'Book a Meeting';
+}
