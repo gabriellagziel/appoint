e3bdbb8abfed1e325cb048096d795af21ac22955
gabriellagziel <gabriellagziel@gmail.com>
2025-06-27 23:19:39 +0200

    test: expand personal app widget tests

--- test/features/personal_app/edit_profile_screen_test.dart
+++ test/features/personal_app/edit_profile_screen_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:appoint/features/personal_app/ui/edit_profile_screen.dart';
+import '../../fake_firebase_setup.dart';
 
 Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  await initializeTestFirebase();
   group('EditProfileScreen', () {
     testWidgets('renders form fields and save button', (tester) async {
       await tester.pumpWidget(const MaterialApp(home: EditProfileScreen()));
