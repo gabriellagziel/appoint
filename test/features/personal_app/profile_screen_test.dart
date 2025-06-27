75e88a5fb39a12826b44190bc346438ebcd39073
gabriellagziel <gabriellagziel@gmail.com>
2025-06-27 23:18:26 +0200

    fix tests and add firebase mocks

--- test/features/personal_app/profile_screen_test.dart
+++ test/features/personal_app/profile_screen_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:appoint/features/personal_app/ui/profile_screen.dart';
+import '../../fake_firebase_setup.dart';
 
 Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  await initializeTestFirebase();
   group('ProfileScreen', () {
     testWidgets('renders avatar, text, and edit button', (tester) async {
       await tester.pumpWidget(const MaterialApp(home: ProfileScreen()));
