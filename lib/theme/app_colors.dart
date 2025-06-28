56b014a5ab047e7e724a8912e3abd1081cd673d3
gabriellagziel <gabriellagziel@gmail.com>
2025-06-28 11:45:59 +0200

    🎨 Centralize AppColors theme config

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1E88E5);
+  static const Color secondary = Color(0xFF42A5F5);
+  static const Color background = Color(0xFFF1F1F1);
+  static const Color error = Colors.red;
+}
