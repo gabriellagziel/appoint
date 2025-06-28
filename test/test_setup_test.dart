95a8e62c18c3807bcb933b2174fd4ef8d45a4d13
gabriellagziel <gabriellagziel@gmail.com>
2025-06-28 16:09:26 +0200

    Add test config and update setups

--- test/test_setup_test.dart
+++ test/test_setup_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
+import 'test_config.dart';
 import 'test_setup.dart';
 
 void main() {
+  setupTestConfig();
   setupFirebaseMocks();
 
   test('setup loads without error', () {
