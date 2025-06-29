d2102f32b40a64f9f9b64bb45ae6569383a04aa2
gabriellagziel <gabriellagziel@gmail.com>
2025-06-29 02:43:33 +0200

    Add booking confirmation bottom sheet

--- lib/features/studio/studio_booking_confirm_screen.dart
+++ lib/features/studio/studio_booking_confirm_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+
+import '../../widgets/bottom_sheet_manager.dart';
+import '../../widgets/booking_confirmation_sheet.dart';
 import 'studio_booking_screen.dart';
 
 class StudioBookingConfirmScreen extends ConsumerWidget {
@@ -21,7 +25,22 @@ class StudioBookingConfirmScreen extends ConsumerWidget {
             Text('Time: ${args.slot.format(context)}'),
             const Spacer(),
             ElevatedButton(
-              onPressed: () => Navigator.pop(context),
+              onPressed: () async {
+                final summaryText = 'You are about to book a session on '
+                    '${DateFormat.yMMMd().format(args.date)} at '
+                    '${args.slot.format(context)} with ${args.staff.displayName}.';
+                await BottomSheetManager.show(
+                  context: context,
+                  child: BookingConfirmationSheet(
+                    summaryText: summaryText,
+                    onCancel: () => Navigator.of(context).pop(),
+                    onConfirm: () {
+                      Navigator.of(context).pop();
+                      Navigator.pop(context);
+                    },
+                  ),
+                );
+              },
               child: const Text('Confirm Booking'),
             ),
           ],
