e642f9cb31e6d8a8e3303d46b0c6be3b41691410
gabriellagziel <gabriellagziel@gmail.com>
2025-06-29 02:24:12 +0200

    feat(notification): add local mock notification service

---
+++ lib/services/local_notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/notification_item.dart';
+
+/// Simple service that manages in-memory notifications.
+class LocalNotificationService {
+  LocalNotificationService(this._controller);
+
+  final StateController<List<NotificationItem>> _controller;
+
+  /// Returns the current list of notifications.
+  List<NotificationItem> fetchNotifications() {
+    return _controller.state;
+  }
+
+  /// Clears all stored notifications.
+  void clearNotifications() {
+    _controller.state = [];
+  }
+}
