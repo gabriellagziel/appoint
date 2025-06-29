9d8b301c27334ef007514120c35010c5330823f9
gabriellagziel <gabriellagziel@gmail.com>
2025-06-29 02:32:12 +0200

    Add test file scaffolds

---
+++ test/features/settings/settings_screen_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('SettingsScreen', () {
+    setUp(() {
+      // TODO: Initialize test dependencies
+    });
+
+    testWidgets('renders correctly', (tester) async {
+      // TODO: implement widget test
+    });
+  });
+}
