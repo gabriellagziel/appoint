06b0c94348599ee668049a72a45c57c8f92fb1b1
gabriellagziel <your-email@example.com>
2025-07-13 04:16:33 +0200

    Temporarily change marketing to service instead of static site

--- marketing/next.config.js
+++ marketing/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // Temporarily removed static export to work as a service
   trailingSlash: true,
   images: {
     unoptimized: true
